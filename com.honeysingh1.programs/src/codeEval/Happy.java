db30cbafb362f05912910b59b249ff296ca4e016
honeysingh1 <singh178@purdue.edu>
2015-02-13 14:31:49 -0500

    new

---
+++ com.honeysingh1.programs/src/codeEval/Happy.java
@@ -0,0 +1,47 @@
+package codeEval;
+
+// Check if number is happy or not
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Happy {
+	public static void main(String[] args) throws IOException {
+		try {
+			File file = new File(args[0]);
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = in.readLine()) != null) {
+				if (!line.isEmpty()) {
+					if(line.equals("1"))
+						System.out.println("1");
+					else
+						check(Integer.parseInt(line));
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("File Read Error: " + e.getMessage());
+		}
+		System.exit(0);
+	}
+
+	private static void check(int x) {
+		ArrayList<Integer> hap = new ArrayList<Integer>();
+		int total = 0;
+		do
+		{
+			hap.add(total);
+			total = 0;
+			while(x > 0)
+			{
+				total += Math.pow(x % 10, 2);
+				x /= 10;	
+			}
+			x = total;
+		}while(!hap.contains(total) && total != 1);
+		if(total == 1)
+			System.out.println("1");
+		else
+			System.out.println("0");
+	}
+}
---
+++ com.honeysingh1.programs/src/codeEval/Unique.java
@@ -0,0 +1,50 @@
+package codeEval;
+
+//checks if integers in one list are available in other
+// both lists in ascending order
+
+import java.io.*;
+
+public class Unique {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File(args[0]);
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = in.readLine()) != null) {
+				if (!line.isEmpty()) {
+					String[] nums = line.split(";");
+					String[] asc = nums[0].split(",");
+					String[] list = nums[1].split(",");
+					StringBuilder st = new StringBuilder();
+					int flag = 0;
+					for(int i = 0; i < asc.length && flag == 0; i++) {
+						for(int j = 0;j < list.length && flag == 0; j++) {
+							if(asc[i].equals(list[j])) {
+								while(asc[i].equals(list[j]))
+								{
+									st.append(asc[i]);
+									st.append(",");
+									if(i < asc.length - 1)
+										i++; 
+									if(j < list.length - 1)
+										j++;
+								}
+								flag = 1;
+							}
+						}
+					}
+					if(st.length() > 0)
+					System.out.println(st.deleteCharAt(st.length() - 1));
+					else
+						System.out.println();
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("File Read Error: " + e.getMessage());
+		}
+		System.exit(0);
+
+	}
+}
